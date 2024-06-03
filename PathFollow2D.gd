27f4d1c831f5d173b063d674987a18a5a74ea796
tbm57 <tbm@52games52weeks.com>
2024-06-03 14:59:22 -0500

    memory leak fix, other cleanup

--- PathFollow2D.gd
+++ PathFollow2D.gd
@@ -1,50 +1,67 @@
 extends PathFollow2D
 
-enum STATES { FORWARD, BACKWARD, PAUSE, PLAY }
+enum EVENTS { FORWARD, BACKWARD, PAUSE, PLAY }
+enum STATES { FORWARD, BACKWARD, PAUSED }
 
-class State:
+class State extends RefCounted:
 	const SPEED = 300
 
 	var node: Node2D
-	var previous_state: State
+	var previous_state: STATES
+	var current_state: STATES
 	
-	func _init(_node: Node2D, _previous_state: State):
+	func _init(_node: Node2D):
 		node = _node
-		previous_state = _previous_state
 		
 	func apply_movement(delta): pass
 	
-	func next(state: STATES) -> State:
-		var result = self
-		match state:
-			STATES.BACKWARD: result = Backward.new(node, self)
-			STATES.FORWARD: result = Forward.new(node, self)
-			STATES.PAUSE: result = Paused.new(node, self)
+	func next(event: EVENTS) -> State:
+		var result
+		match event:
+			EVENTS.FORWARD: result = Forward.new(node)
+			EVENTS.BACKWARD: result = Backward.new(node)
+			EVENTS.PAUSE: result = Paused.new(node, current_state)
+			_: result = Forward.new(node)
 		return result
 		
 class Forward extends State:
+	func _init(_node: Node2D):
+		super(_node)
+		current_state = STATES.FORWARD
+	
 	func apply_movement(delta):
 		node.progress += SPEED * delta
 
 class Backward extends State:
+	func _init(_node: Node2D):
+		super(_node)
+		current_state = STATES.BACKWARD
+	
 	func apply_movement(delta):
 		node.progress -= SPEED * delta
 		
 class Paused extends State:
-	func apply_movement(delta):
-		pass
+	func _init(_node: Node2D, _previous_state: STATES):
+		super(_node)
+		previous_state = _previous_state
+		current_state = STATES.PAUSED
 		
-	func next(state: STATES) -> State:
-		if (state == STATES.PLAY):
-			return previous_state
+	func next(event: EVENTS) -> State:
+		if (event == EVENTS.PLAY):
+			if (previous_state == STATES.FORWARD):
+				return Forward.new(node)
+			elif (previous_state == STATES.BACKWARD):
+				return Backward.new(node)
+			else:
+				return Paused.new(node, current_state)
 		else:
-			return super(state)
+			return super(event)
 	
-var current_state = Forward.new(self, null)
+var current_state = Forward.new(self)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	current_state.apply_movement(delta)
 	
-func change_state(next_state):
-	current_state = current_state.next(next_state)
+func change_state(event: EVENTS):
+	current_state = current_state.next(event)
